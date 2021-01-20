93bf70dd1b9eb83041bd4a707fe3426995b2ca0e
Sam Gold <samg915757@gmail.com>
2021-01-20 14:50:22 -0700

    all seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,96 @@
+Display.destroy_all
+
 Display.create(
-    name: "Old Train",
-    image: "https://lh3.googleusercontent.com/proxy/L6Ta0oKDq6gJAwKIudC3_sr1VSXi_ga9zJ7NRML8-rgJsBjC8InQcopfHz29eNTF9hgj10gC6E2daSjOQNNejK4wrviRInA3bWPghywZjdur1z5yhEzAIvu4aQ1mtdYOT-62pnYd4H20_Iwhh_S4bpaGg2J9NxrMH-uHLEXyX7KtBH9uQ1oLAcwQxQLrWc4MhN99KE7ZGV0YG2s2XXyRAmD12PysUgAgGew0Kf0W9nXxEtzsXA",
-    caption: "On September 27, 1825, Locomotion No. 1 became the world's first steam locomotive to carry passengers on a public line"
+    title: "Crawling Babies",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmagazine.ackermans.co.za%2Fwp-content%2Fuploads%2F2018%2F09%2FCrawling_feature-700x900px.jpg&f=1&nofb=1",
+    caption: "Knowing one must crawl before one can walk, we see three eager students watching the 'How To Walk' tutorial video." 
 )
+
+Display.create(
+    title: "Baby's First Steps B&W",
+    image: "https://i.etsystatic.com/5929390/r/il/fe587b/1311477304/il_570xN.1311477304_cz3o.jpg",
+    caption: "Human's first steps have changed little over the decades."
+)
+
+Display.create(
+    title: "Baby's First Steps (Present Day)",
+    image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fassets.babycenter.com%2Fims%2F2012%2F08%2Fintl_india_andrea_Gupta_203.jpg&f=1&nofb=1", 
+    caption: "It's not overkill; it's just adorable."
+)
+
+Display.create(
+    title: "Old Train",
+    image: "https://lh3.googleusercontent.com/proxy/L6Ta0oKDq6gJAwKIudC3_sr1VSXi_ga9zJ7NRML8-rgJsBjC8InQcopfHz29eNTF9hgj10gC6E2daSjOQNNejK4wrviRInA3bWPghywZjdur1z5yhEzAIvu4aQ1mtdYOT-62pnYd4H20_Iwhh_S4bpaGg2J9NxrMH-uHLEXyX7KtBH9uQ1oLAcwQxQLrWc4MhN99KE7ZGV0YG2s2XXyRAmD12PysUgAgGew0Kf0W9nXxEtzsXA",
+    caption: "On September 27, 1825, Locomotion No. 1 became the world's first steam locomotive to carry passengers on a public line."
+)
+
+Display.create(
+    title: "Old Train Today",
+    image: "https://media-cdn.tripadvisor.com/media/photo-s/13/f2/a6/72/locomotion-1.jpg",
+    caption: "After transporting many passengers 3 at a time, Locomotion No. 1 has since been enshrined."
+)
+
+Display.create(
+    title: "Bullet Train",
+    image:"https://www.greenbiz.com/sites/default/files/images/articles/featured/highspeedrailsstock.jpg",
+    caption: "We've come a long way with trains (more than 200km/hour!)"
+)
+
+Display.create(
+    title: "First Flight",
+    image: "https://wwwcache.wral.com/asset/news/state/2018/12/17/18070958/10911-bb5c14a48ced4e8a974087bf2cadc297-DMID1-5h5zfvp93-640x480.jpg",
+    caption: "First flight of Wright Brother's Airplane, 1903." 
+)
+
+Display.create(
+    title: "Boeing 757",
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fairwaysmag.com%2Fwp-content%2Fuploads%2F2016%2F08%2FBoeing_757-200_Farnborough_1982_Fitzgerald-680x365_c.jpg&f=1&nofb=1",
+    caption: "Despite being much faster, present day planes actually have less legroom than that of a 757"
+)
+
+Display.create(
+    title: "Original Mercedes",
+    image: "https://www.carscoops.com/wp-content/uploads/2018/06/17e4665d-benz-patent-motorwagen-.jpg",
+    caption: "Replica of first Mercedes-Benz, acknowledged as first car ever made by Karl Benz." 
+)
+
+Display.create(
+    title: "Model T",
+    image: "https://carsguide-res.cloudinary.com/image/upload/f_auto%2Cfl_lossy%2Cq_auto%2Ct_default/v1/editorial/story/hero_image/1908-Ford-Model-T_0.jpg",
+    caption: "Model T, created by Henry Ford, 1908." 
+)
+
+Display.create(
+    title: "Pontiac Aztek",
+    image: "https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/640x400/quality/80/https://s.aolcdn.com/commerce/autodata/images/CAB50POS011A0101.jpg",
+    caption: "The Pontiac Aztek, considered the ugliest car ever created, was driven by Walter White." 
+)
+
+Display.create(
+    title: "Thomas The Six Wheeled Hatchback",
+    image: "https://pbs.twimg.com/media/DRAXW0LXkAAtFg6.jpg",
+    caption: "Sometimes hybridization can lead to undesirable results."
+)
+
+Display.create(
+    title: "Unicycle",
+    image: "https://i.ebayimg.com/images/g/b1QAAMXQq8BQ7Fhx/s-l1600.jpg",
+    caption: "This poser needs a training wheel for his unicycle, 1891."
+)
+
+Display.create(
+    title: "Razor Scooter",
+    image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.couponersunited.com%2Fwp-content%2Fuploads%2F2017%2F02%2FScreen-Shot-2017-02-09-at-10.14.13-AM.png&f=1&nofb=1",
+    caption: "YoU kNoW yOu WeRe A 9oS kId iF this is the hottest item on two wheels."
+)
+
+Display.create(
+    title: "Segway",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fco2calculator.files.wordpress.com%2F2008%2F10%2Fsegway.jpg&f=1&nofb=1",
+    caption: "Also know as 'a Razor with a Hemi', the creator of the Segway died in a Segway accident."
+)
+
+
+
+
+
